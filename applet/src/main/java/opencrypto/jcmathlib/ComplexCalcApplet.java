c86a3786b1d485131656cb43ecaf6f6295587155
cryptoapplicantanon17-cell <crypto.applicant.anon17@gmail.com>
2026-03-23 17:44:51 +0100

    Update ComplexCalcApplet.java
    Anon22

--- applet/src/main/java/opencrypto/jcmathlib/ComplexCalcApplet.java
+++ applet/src/main/java/opencrypto/jcmathlib/ComplexCalcApplet.java
@@ -7,7 +7,7 @@
  * * Dependencies:
  * - JCMathLib (https://github.com/OpenCryptoProject/JCMathLib)
  * * License: MIT License
- * Copyright (c) 2026 Mohamed BACHKAT
+ * Copyright (c) 2026 Anon22
  * * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
